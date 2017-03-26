321b3f2a340b014f12db112f783ddf1e0ae33160
AlexeyKorkoza <alexeykorkoza@gmail.com>
2017-03-26 23:34:44 +0300

    Add feedback

--- js/main.js
+++ js/main.js
@@ -1,8 +1,36 @@
-$(document).ready(function(){
-  $("#nav-icon").click(function(){
-    $(this).toggleClass("open");
-    $("#navigation").toggle();
-  });
+$(document).ready(function() {
+    $("#nav-icon").click(function() {
+        $(this).toggleClass("open");
+        $("#navigation").toggle();
+    });
 });
-var divs = $("div");
-var amazingslider_box = $(".amazingslider-box-1").find(divs);
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        900: {
+            items: 3
+        }
+    }
+});
+
+$(".owl-prev").empty();
+$(".owl-next").empty();
+$(".owl-dots").remove();
+
+var childs = $('.amazingslider-box-1').children();
+console.log(childs);
+childs.each(function(i, e) {
+    if ($(e).attr('class') === undefined) {
+        $(e).remove();
+    }
+});
