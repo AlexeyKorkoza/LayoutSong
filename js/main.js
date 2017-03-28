6f0efca369eb8e6b6f67bc87bc3b34d45ef5016a
AlexeyKorkoza <alexeykorkoza@gmail.com>
2017-03-28 18:08:03 +0300

    Add answer-question 320

--- js/main.js
+++ js/main.js
@@ -27,6 +27,22 @@ $(".owl-prev").empty();
 $(".owl-next").empty();
 $(".owl-dots").remove();
 
+var acc = document.getElementsByClassName("answer_question_accordion");
+var acc1 = document.getElementsByClassName("arrow-bottom");
+
+for (var i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            document.getElementsByClassName("arrow-bottom")[0].style.transition = "0.4s";
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    }
+}
+
 var childs = $('.amazingslider-box-1').children();
 console.log(childs);
 childs.each(function(i, e) {
