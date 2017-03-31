b102d622c2c219f88e672b1dc0dbde559f360fe2
AlexeyKorkoza <alexeykorkoza@gmail.com>
2017-03-31 14:06:07 +0300

    Add slider in tabs of promo-music

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
     $("#nav-icon").click(function() {
         $(this).toggleClass("open");
-        $("#navigation").toggle();
+        $(".navigation").toggle();
     });
 });
 
-$('.owl-carousel').owlCarousel({
+/* feedback-slider */
+$('.feedback_slider .owl-carousel').owlCarousel({
     loop: true,
     margin: 10,
     nav: true,
@@ -23,15 +24,31 @@ $('.owl-carousel').owlCarousel({
     }
 });
 
-$(".owl-prev").empty();
-$(".owl-next").empty();
-$(".owl-dots").remove();
+$(".feedback_slider .owl-prev").empty();
+$(".feedback_slider .owl-next").empty();
+$(".feedback_slider .owl-dots").remove();
+
+/* promo_feedback-slider */
+$('.promo_feedback .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1
+        }
+    }
+});
+
+$(".promo_feedback .owl-prev").empty();
+$(".promo_feedback .owl-next").empty();
+$(".promo_feedback .owl-dots").remove();
 
 var acc = document.getElementsByClassName("answer_question_accordion");
-var acc1 = document.getElementsByClassName("arrow-bottom");
 
 for (var i = 0; i < acc.length; i++) {
-    acc[i].onclick = function(){
+    acc[i].onclick = function() {
         this.classList.toggle("active");
         var panel = this.nextElementSibling;
         if (panel.style.display === "block") {
@@ -41,6 +58,20 @@ for (var i = 0; i < acc.length; i++) {
             panel.style.display = "block";
         }
     }
+}
+
+function openWhom(evt, whom) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("promo_feedback");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(whom).style.display = "block";
+    evt.currentTarget.className += " active";
 }
 
 var childs = $('.amazingslider-box-1').children();
